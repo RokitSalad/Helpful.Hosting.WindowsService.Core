5e894bf44e244c65c4b0741030e3499a7ccda5aa
rokitsalad <peterdobbs@hotmail.com>
2020-06-06 16:19:22 +1000

    moving install logic into azure yml file - not sure I like that, feels a bit specific to azure devops.

--- src/DemoServiceBackgroundProcess/Tools/Deploy.ps1
+++
@@ -1,8 +0,0 @@
-$Service = "$PSScriptRoot\..\DemoServiceBackgroundProcess.exe"
-& $Service stop
-Start-Sleep -Seconds 5
-& $Service uninstall
-Start-Sleep -Seconds 5
-& $Service install
-Start-Sleep -Seconds 5
-& $Service Start
