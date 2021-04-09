90cd661bd1dfcff0cc3029c9bfe393079138de9e
rokitsalad <peterdobbs@hotmail.com>
2021-04-09 15:17:45 +1000

    added swagger

--- src/Helpful.Hosting.WorkerService/DefaultWebStartup.cs
+++ src/Helpful.Hosting.WorkerService/DefaultWebStartup.cs
@@ -1,13 +1,18 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace Helpful.Hosting.WorkerService
 {
     public class DefaultWebStartup
     {
+        private static string ApplicationTitle => Assembly.GetEntryAssembly()?.GetName().ToString();
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = ApplicationTitle, Version = "v1" }); });
         }
 
         public void Configure(IApplicationBuilder app)
@@ -16,7 +21,9 @@ namespace Helpful.Hosting.WorkerService
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
+            }); 
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{ApplicationTitle} V1"); });
         }
     }
 }
